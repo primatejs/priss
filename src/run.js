a82f691ceb93761389f4099076fe08f3df096bd2
phaleth <29838606+phaleth@users.noreply.github.com>
2023-04-07 14:04:41 +0200

    Remove async from run func

--- src/run.js
+++ src/run.js
@@ -4,6 +4,6 @@ import * as config from "./config.js";
 
 const command = name => commands[name] ?? commands.help;
 const serveOrCreateConfig = async () => commands[await config.exists() ? "dev" : "create"];
-const run = async name => name === undefined ? serveOrCreateConfig() : command(name);
+const run = name => name === undefined ? serveOrCreateConfig() : command(name);
 
 (await run(args[0]))();
