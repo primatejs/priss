84cd3dd658f718bbc71fe3bb57380a594c12018b
terrablue <102580937+terrablue@users.noreply.github.com>
2023-04-09 17:42:08 +0300

    use the new simplified module interface

---
+++ README.md
@@ -0,0 +1,5 @@
+# Priss
+
+## Install
+
+`npx priss`
--- src/module.js
+++ src/module.js
@@ -2,38 +2,37 @@ import {marked} from "marked";
 import {Path} from "runtime-compat/fs";
 import svelte from "@primate/svelte";
 
-export default config => async env => {
-  const root = env.root.join(config.root);
-  const path = new Path(import.meta.url).directory.directory.join("components");
-  return {
-    load: () => svelte(path),
-    route: async (request, next) => {
-      const {pathname} = request;
-      // check if an .md file exists at this path
-      const md = root.join(`${pathname}.md`);
-      if (await md.exists) {
-        const toc = [];
-        const renderer = {
-          heading(text, level) {
-            const name = text.toLowerCase().replace(/[^\w]+/gu, "-");
-            toc.push({level, text, name});
+const path = new Path(import.meta.url).directory.directory.join("components");
+export default config => ({
+  load() {
+    return svelte(path);
+  },
+  async route({request, env}, next) {
+    const {pathname} = request;
+    // check if an .md file exists at this path
+    const md = env.root.join(config.root).join(`${pathname}.md`);
+    if (await md.exists) {
+      const toc = [];
+      const renderer = {
+        heading(text, level) {
+          const name = text.toLowerCase().replace(/[^\w]+/gu, "-");
+          toc.push({level, text, name});
 
-            return `
-              <h${level}>
-                <a name="${name}" class="anchor" href="#${name}">
-                  <span class="header-link"></span>
-                </a>
-                ${text}
-              </h${level}>
-            `;
-          },
-        };
-        marked.use({renderer});
-        const app = config;
-        const content = marked.parse(await md.file.read());
-        return env.handlers.svelte("StaticPage", {content, toc, app});
-      }
-      return next(request);
-    },
-  };
-};
+          return `
+            <h${level}>
+              <a name="${name}" class="anchor" href="#${name}">
+                <span class="header-link"></span>
+              </a>
+              ${text}
+            </h${level}>
+          `;
+        },
+      };
+      marked.use({renderer});
+      const app = config;
+      const content = marked.parse(await md.file.read());
+      return env.handlers.svelte("StaticPage", {content, toc, app});
+    }
+    return next({request, env});
+  },
+});
