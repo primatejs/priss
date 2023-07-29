ae4f4145e27644ab83772b15fe6cf9a4e991c17c
terrablue <102580937+terrablue@users.noreply.github.com>
2023-07-29 20:53:22 +0300

    add error page

---
+++ components/Errorpage.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Header from "./Header.svelte";
+
+  export let data;
+
+  const {app} = data;
+</script>
+<Header {app}/>
+<article>
+  <h1>Page not found</h1>
+
+  <p>
+    This page does not exist.
+  </p>
+</article>
