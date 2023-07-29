a07678a34bd1d5b64826bbb47c0343e5f09d883d
terrablue <102580937+terrablue@users.noreply.github.com>
2023-07-29 21:24:24 +0300

    refine error page

--- components/Errorpage.svelte
+++ components/Errorpage.svelte
@@ -1,15 +1,23 @@
 <script>
+  import {onMount} from "svelte";
   import Header from "./Header.svelte";
 
   export let data;
+  let location = "";
+
+  onMount(() => {
+    location = window.location.pathname;
+  });
 
   const {app} = data;
 </script>
 <Header {app}/>
+<main>
 <article>
   <h1>Page not found</h1>
 
   <p>
-    This page does not exist.
+    The requested page <strong>{location}</strong> does not exist.
   </p>
 </article>
+</main>
